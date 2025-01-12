65da22535c06a07b09b71d730804d7172b0b1906
Chinmaya Narmdeshwar <chinmayanarmdeshwar@gmail.com>
2025-01-12 23:56:21 +0530

    Refactor CUDA device information retrieval in (4)cuda_devive_info.cu for clarity and conciseness

--- PART B (CUDA)/(4)cuda_devive_info.cu
+++ PART B (CUDA)/(4)cuda_devive_info.cu
@@ -1,37 +1,22 @@
+#include <cuda_runtime.h>
 #include <stdio.h>
-#include <cuda.h>
 
 int main() {
-    int deviceCount;
-    cudaGetDeviceCount(&deviceCount); // Get the number of CUDA-capable devices
+    cudaDeviceProp deviceProp;
+    int devID;
 
-    printf("Number of CUDA devices: %d\n", deviceCount);
+    cudaGetDevice(&devID);
+    cudaGetDeviceProperties(&deviceProp, devID);
 
-    for (int device = 0; device < deviceCount; device++) {
-        cudaDeviceProp deviceProp;
-        cudaGetDeviceProperties(&deviceProp, device);
-
-        printf("\nDevice %d: %s\n", device, deviceProp.name);
-        printf("  Compute capability: %d.%d\n", deviceProp.major, deviceProp.minor);
-        printf("  Total global memory: %lu bytes\n", deviceProp.totalGlobalMem);
-        printf("  Shared memory per block: %lu bytes\n", deviceProp.sharedMemPerBlock);
-        printf("  Registers per block: %d\n", deviceProp.regsPerBlock);
-        printf("  Warp size: %d\n", deviceProp.warpSize);
-        printf("  Maximum threads per block: %d\n", deviceProp.maxThreadsPerBlock);
-        printf("  Maximum threads per multiprocessor: %d\n", deviceProp.maxThreadsPerMultiProcessor);
-        printf("  Number of multiprocessors: %d\n", deviceProp.multiProcessorCount);
-        printf("  Maximum grid dimensions: %d x %d x %d\n",
-               deviceProp.maxGridSize[0],
-               deviceProp.maxGridSize[1],
-               deviceProp.maxGridSize[2]);
-        printf("  Maximum block dimensions: %d x %d x %d\n",
-               deviceProp.maxThreadsDim[0],
-               deviceProp.maxThreadsDim[1],
-               deviceProp.maxThreadsDim[2]);
-        printf("  Clock rate: %d kHz\n", deviceProp.clockRate);
-        printf("  Memory clock rate: %d kHz\n", deviceProp.memoryClockRate);
-        printf("  Memory bus width: %d bits\n", deviceProp.memoryBusWidth);
-    }
+    printf("CUDA Device Information:\n\n");
+    printf("1. Warp size: %d\n", deviceProp.warpSize);
+    printf("2. Maximum number of threads per multiprocessor: %d\n", deviceProp.maxThreadsPerMultiProcessor);
+    printf("3. Maximum number of threads per block: %d\n", deviceProp.maxThreadsPerBlock);
+    printf("4. Maximum sizes of each dimension of a block: x = %d, y = %d, z = %d\n",
+           deviceProp.maxThreadsDim[0], deviceProp.maxThreadsDim[1], deviceProp.maxThreadsDim[2]);
+    printf("5. Maximum sizes of each dimension of a grid: x = %d, y = %d, z = %d\n",
+           deviceProp.maxGridSize[0], deviceProp.maxGridSize[1], deviceProp.maxGridSize[2]);
+    printf("6. Maximum memory pitch: %zu bytes\n", deviceProp.memPitch);
 
     return 0;
 }
